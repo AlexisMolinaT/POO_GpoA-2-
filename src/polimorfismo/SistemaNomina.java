eae842680eb01b145caf11cbe3a0e20b464b3a28
invitado <invitado@LABA010209.itchii.lab>
2022-04-30 01:37:38 -0600

    Act pol

--- src/polimorfismo/SistemaNomina.java
+++ src/polimorfismo/SistemaNomina.java
@@ -9,15 +9,19 @@ public class SistemaNomina {
 
         EmpleadoAsalariado empleadoAsalariado = new EmpleadoAsalariado("Alexis"," Molina","123",800.00);
         EmpleadoBaseMasComision empleadoBaseMasComision = new EmpleadoBaseMasComision ("Ricardo"," Molina","720",2000,0.2,1900);
-        EmpleadoPorComision empleadoPorComisiom = new EmpleadoPorComision ("Fernando"," Esparza","930",600,0.3);
+        EmpleadoPorComision empleadoPorComision = new EmpleadoPorComision ("Fernando"," Esparza","930",600,0.3);
         EmpleadoPorHoras empleadoPorHoras = new EmpleadoPorHoras ("Valeria"," Perez","321",16.75,40);
         
         System.out.printf("%s\n%s: $%,.2f\n\n",empleadoAsalariado,"ingresos",empleadoAsalariado.ingresos());
         System.out.printf("%s\n%s: $%,.2f\n\n",empleadoBaseMasComision,"ingresos",empleadoBaseMasComision.ingresos());
-        System.out.printf("%s\n%s: $%,.2f\n\n",empleadoPorComisiom,"ingresos",empleadoPorComisiom.ingresos());
+        System.out.printf("%s\n%s: $%,.2f\n\n",empleadoPorComision,"ingresos",empleadoPorComision.ingresos());
         System.out.printf("%s\n%s: $%,.2f\n\n",empleadoPorHoras,"ingresos",empleadoPorHoras.ingresos());
         
         Empleado[] empleados = new Empleado[4];
+        empleados[0]=empleadoAsalariado;
+        empleados[1]=empleadoBaseMasComision;
+        empleados[2]=empleadoPorComision;
+        empleados[3]=empleadoPorHoras;
 
     }
     }
